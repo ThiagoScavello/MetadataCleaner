3d5927a95393ea8663b4e5dfeeab6e082b885816
Thiago Scavello <scavellothiago@gmail.com>
2024-05-24 22:37:14 -0300

    start

---
+++ src/main/java/com/ghostnet/MetadataCleanerApplication.java
@@ -0,0 +1,13 @@
+package com.ghostnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetadataCleanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetadataCleanerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ghostnet/MetadataCleanerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghostnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetadataCleanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
